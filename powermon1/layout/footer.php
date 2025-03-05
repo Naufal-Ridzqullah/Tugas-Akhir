e08d97e9543c29a3f313c736539c105b823914f3
Naufal-Ridzqullah <naufalridzqullah@gmail.com>
2025-03-05 21:31:11 +0700

    delete old

--- powermon1/layout/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="main-footer bg-dark d-flex justify-content-center align-items-center" style="height: 50px;">
-    <p class="text-white mb-0">
-        Copyright &copy; PMS All rights reserved.
-    </p>
-</footer>
